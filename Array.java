9085ae41166f4578608e87da989596c4871cb171
harshaldangare <142479213+harshaldangare@users.noreply.github.com>
2023-08-19 12:28:35 +0530

    Create Array.java
    array

---
+++ Array.java
@@ -0,0 +1,19 @@
+class Array{
+ 
+   public static void main(String[] arg) {
+  
+  
+        String name[] = {"shyam","ram","sita","gita"};
+  
+      int number[]={24,23,27,28,20};
+  
+      System.out.println(name[3]);
+  
+      for (int i=0;i<=number.length;i++)
+ {    
+   System.out.println(number[i]);
+  
+      
+        }
+}
+}
